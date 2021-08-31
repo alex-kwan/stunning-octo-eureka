53398c1b8c6c27d3dd34aae724ca036856318fbd
alex-kwan <alexkwan089@gmail.com>
2021-08-30 20:33:15 -0700

    pushing in some additional changes

--- Controllers/AddUserController.cs
+++ Controllers/AddUserController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Azure.Communication.Identity;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Azure.Communication.CallingServer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace my_new_app.Controllers
 {
@@ -21,26 +23,73 @@ namespace my_new_app.Controllers
         }
 
         [HttpGet]
-        public async Task<Object> Get()
+        public async Task Get([FromQuery] string serverCallId)
         {
-            string connectionString = _config.GetValue<string>("ResourceConnectionString");
-            
-            System.Console.WriteLine("My connection string is "+connectionString);
-
+            var connectionString = _config.GetValue<string>("ResourceConnectionString");
             var client = new CommunicationIdentityClient(connectionString);
 
-            var tokenResponse = await client.CreateUserAndTokenAsync(scopes: new [] { CommunicationTokenScope.VoIP });
-            var token =  tokenResponse.Value.AccessToken.Token;
-            var expiresOn = tokenResponse.Value.AccessToken.ExpiresOn;
-            var user = tokenResponse.Value.User;
-            Console.WriteLine($"\nIssued an access token with 'voip' scope that expires at {expiresOn}:");
-            Console.WriteLine(token);
+            // user added and removed from the call
+            var communicationUserIdentifierResponse = await client.CreateUserAsync();
+            var user =  communicationUserIdentifierResponse.Value;
+
+            serverCallId = "5013c8f1-fd98-44d9-9f0a-ebbb5790b253";
+            var baseUrl = Request.Host.ToString(); // "https://e970-2001-569-7c3a-8700-7cd8-3eab-7510-427d.ngrok.io";
+            Console.WriteLine("Request Host = "+Request.Protocol + "://"+baseUrl);
             
-            return new {
-                token,
-                expiresOn,
-                user
-            };
+            var callingServerClient = new CallingServerClient(connectionString);
+            var callbackUrl = new Uri(baseUrl +"/api/CallbackTest");
+
+            var mediaTypes = new[] {MediaType.Audio};
+            var eventSubscriptionTypes = new[] { EventSubscriptionType.ParticipantsUpdated };
+            var uri = new Uri(baseUrl+"/MyStaticFiles/test1.wav");
+
+            var joinCallOptions = new JoinCallOptions(callbackUrl, mediaTypes, eventSubscriptionTypes);
+            try { 
+                
+                var serverCall = callingServerClient.InitializeServerCall(serverCallId);
+                try {
+                    System.Threading.Thread.Sleep(5000);
+                    var result = await serverCall.PlayAudioAsync(uri, Guid.NewGuid().ToString(), callbackUrl);
+                }
+                catch(Exception ex) {
+                    System.Console.WriteLine("trying to play audio from server call "+ex.ToString());
+                }
+                var callConnectionResponse = await callingServerClient.JoinCallAsync(serverCallId, user, joinCallOptions);
+                var callConnection = callConnectionResponse.Value;
+
+                Console.WriteLine("My call connection id for group call ("+serverCallId.ToString() +")= " +callConnection.CallConnectionId);
+                Console.WriteLine("Added User1 "+user.Id);
+
+                try {
+                    System.Threading.Thread.Sleep(30000);
+                    System.Console.WriteLine("Playing audio from uri = "+uri.ToString());
+
+                    var playAudioRequest = new PlayAudioOptions()
+                    {
+                        AudioFileUri = uri,
+                        OperationContext = Guid.NewGuid().ToString(),
+                        Loop = true,
+                    };
+
+                    var response = await callConnection.PlayAudioAsync(playAudioRequest).ConfigureAwait(false);
+
+                   System.Console.WriteLine($"PlayAudioAsync response --> {response.GetRawResponse()}, Id: {response.Value.OperationId}, Status: {response.Value.Status}, OperationContext: {response.Value.OperationContext}, ResultInfo: {response.Value.ResultInfo}");
+
+                    System.Threading.Thread.Sleep(5000);
+                }
+                catch(Exception ex) {
+                    System.Console.WriteLine(ex.ToString());
+                }
+                
+                System.Threading.Thread.Sleep(5000);
+
+                await callConnection.HangupAsync();
+                Console.WriteLine("Removed User "+user.Id);
+               System.Console.WriteLine("done");
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
---
+++ Controllers/CallUserController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Azure.Communication.Identity;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Azure.Communication.CallingServer;
+using System.Collections.Generic;
+using Azure.Communication;
+
+namespace my_new_app.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CallUserController : ControllerBase
+    {
+        private readonly ILogger<CallUserController> _logger;
+        private IConfiguration _config;
+
+        public CallUserController(IConfiguration config, ILogger<CallUserController> logger)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task Get([FromQuery] string id)
+        {
+            String connectionString = _config.GetValue<string>("ResourceConnectionString");
+            var client = new CommunicationIdentityClient(connectionString);
+
+            var communicationUserIdentifierResponse = await client.CreateUserAsync();
+            var user =  communicationUserIdentifierResponse.Value;
+
+            var baseUrl = Request.Host.ToString(); // || "https://e970-2001-569-7c3a-8700-7cd8-3eab-7510-427d.ngrok.io";
+            Console.WriteLine("Request Host = "+Request.Scheme + "://"+baseUrl);
+
+            var callbackUri = baseUrl+"/callback";
+            var uri = new Uri(baseUrl+"/MyStaticFiles/test1.wav");
+            
+            var createCallOption = new CreateCallOptions(
+            new Uri(callbackUri),
+            new[] { MediaType.Audio },
+            new[]
+            {
+                EventSubscriptionType.ParticipantsUpdated,
+                EventSubscriptionType.DtmfReceived
+            });
+
+            var targetUser = new CommunicationUserIdentifier(id);
+
+            var callingServerClient = new CallingServerClient(connectionString);
+
+            var callConnection = await callingServerClient.CreateCallConnectionAsync(
+                user,
+                new List<CommunicationUserIdentifier>() {targetUser},
+                createCallOption
+            );
+
+            var actualCallConnection = callConnection.Value;
+
+            System.Console.WriteLine("Going to wait 30 seconds to wait for the call");
+
+            System.Threading.Thread.Sleep(15000);
+            System.Console.WriteLine("Playing audio from uri = "+uri.ToString());
+
+            var playAudioRequest = new PlayAudioOptions()
+            {
+                AudioFileUri = uri,
+                OperationContext = Guid.NewGuid().ToString(),
+                Loop = true,
+            };
+
+             var response = await actualCallConnection.PlayAudioAsync(playAudioRequest).ConfigureAwait(false);
+
+            System.Console.WriteLine($"PlayAudioAsync response --> {response.GetRawResponse()}, Id: {response.Value.OperationId}, Status: {response.Value.Status}, OperationContext: {response.Value.OperationContext}, ResultInfo: {response.Value.ResultInfo}");
+                
+        }
+    }
+}
